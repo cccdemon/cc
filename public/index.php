b0b7b366fd755699ab8036b9d2ebad0d05d521da
Nick <nick.pastor@reality-bytes.com>
2016-04-19 15:33:21 +0200

    rechtschreibkorrekturen

--- public/index.php
+++ public/index.php
@@ -84,7 +84,7 @@ if(isset($_REQUEST['submit'])) {
             Wir sind ein engagiertes Dienstleistungsunternehmen, das <strong>im gesamten Kölner Raum, sowie im angrenzenden Umland</strong> tätig ist. Mehrjährige Berufserfahrung in anderen Unternehmen hat uns inspiriert ein Unternehmen zu gründen, welches durch dienstleistungsorientiertes Denken und Handeln bestimmt ist. Vielfältige Erfahrungen aus Theorie und Praxis zeigen, dass die <strong>ständige Betreuung und Pflege</strong> des Kunden ein wichtiges Kriterium ist, um eine auf Dauer zufrieden stellende Dienstleistung zu erzielen. Ihre Wünsche und Bedürfnisse sind der Maßstab für unsere Arbeit. Ihre Aufträge erledigen wir <strong>kompetent, zuverlässig, flexibel</strong> und mit dem <strong>Bewusstsein für Umweltschutz und Qualität</strong>.
           </p>
           <p>
-            Die <strong>langfristige Zufriedenheit</strong> unserer Geschäftspartner wird von uns intensiv gepflegt. Das Vertrauen, das wir erhalten, ist die Grundlage unserer Verantwortung. Eine <strong>ehrliche, partnerschaftliche Zusammenarbeit</strong> ist für uns selbstverständlich. <strong>Kompetente Gebäudereinigung</strong> sichert den <strong>langfristigen Werterhalt</strong> und verleiht Immobilien innen und außen einen repräsentativen Charakter. Sollten auch Sie der Überzeugung sein, dass unser Unternehmen Ihnen ein guter und zuverlässiger Partner sein könnte, würden wir uns Ihnen gerne in einem persönlichen Gespräch ausführlich vorstellen.
+            Die <strong>langfristige zufriedenheit</strong> unserer Geschäftspartner wird von uns intensiv gepflegt. Das Vertrauen, das wir erhalten, ist die Grundlage unserer Verantwortung. Eine <strong>ehrliche, partnerschaftliche Zusammenarbeit</strong> ist für uns selbstverständlich. <strong>Kompetente Gebäudereinigung</strong> sichert den <strong>langfristigen Werterhalt</strong> und verleiht Immobilien innen und außen einen repräsentativen Charakter. Sollten auch Sie der Überzeugung sein, dass unser Unternehmen Ihnen ein guter und zuverlässiger Partner sein könnte, würden wir uns Ihnen gerne in einem persönlichen Gespräch ausführlich vorstellen.
           </p>
         </div>
       </section>
@@ -98,7 +98,7 @@ if(isset($_REQUEST['submit'])) {
               <div class="inner">
                 <h4>Unterhaltsreinigung</h4>
                 <p>
-                Wir Reinigen Büros, Praxen, Banken etc. in den von Ihnen gewünschten Zeitabständen.Ob Täglich Wöchentlich oder Monatlich, vom Saugen über Staubwischen bis hin zu den Sanitären Einrichtungen, wir sind für Sie da!
+                Wir reinigen Büros, Praxen, Banken etc. in den von Ihnen gewünschten Zeitabständen.Ob täglich, wöchentlich oder monatlich, vom saugen über Staubwischen bis hin zu den Sanitären Einrichtungen; wir sind für Sie da.
                 </p>
               </div>
             </article>
@@ -108,7 +108,7 @@ if(isset($_REQUEST['submit'])) {
                 <h4>Glasreinigung</h4>
                 <p>
                   Die Glasreinigung ist einer der wichtigsten Bestandteile im äußerlichen Auftreten von Objekten.
-Wir reinigen sämtliche Glasfronten und Variationen, vom Privathaushalt über Schaufenster bis hin zu Großflächen, mit Leitern, Stangen oder Hebebühnen, wir garantieren einen klaren Blick.
+Wir reinigen sämtliche Glasfronten und Variationen, vom Privathaushalt über Schaufenster bis hin zu Großflächen, mit Leitern, Stangen oder Hebebühnen. Wir garantieren einen klaren Blick.
                 </p>
               </div>
             </article>
@@ -117,7 +117,7 @@ Wir reinigen sämtliche Glasfronten und Variationen, vom Privathaushalt über Sc
               <div class="inner">
                 <h4>Treppenhausreinigung</h4>
                 <p>
-                  Jeder freut sich über ein schön gepflegtes Treppenhaus. Saubere Treppen und Podeste, keine Spinnweben, Saubere Keller sowie Waschküchen und gepflegte Hauseingänge. Damit auch Sie dieses Gefühl erleben können, geben Sie uns Ihr Vertrauen. Wir machen es möglich.
+                  Jeder freut sich über ein schön gepflegtes Treppenhaus. Saubere Treppenstufen und Podeste, keine Spinnweben, saubere Keller sowie Waschküchen und gepflegte Hauseingänge. Damit auch Sie dieses Gefühl erleben können, geben Sie uns Ihr Vertrauen. Wir machen es möglich.
                 </p>
               </div>
             </article>
@@ -144,7 +144,7 @@ Wir reinigen sämtliche Glasfronten und Variationen, vom Privathaushalt über Sc
               <div class="inner">
                 <h4>Teppichreinigung</h4>
                 <p>
-                  Dreck von Schuhen, umgestoßenen Gläsern, schwere Verschmutzungen, wir reinigen sämtliche Teppiche sowie Polster und werten diese wieder auf. Auch Sie sollten sich darüber keine Sorgen machen.
+                  Schmutz von Schuhen, umgestoßenen Gläsern, schwere Verschmutzungen, wir reinigen sämtliche Teppiche sowie Polster und werten diese wieder auf. Auch Sie sollten sich darüber keine Sorgen machen.
                 </p>
               </div>
             </article>
@@ -156,7 +156,7 @@ Wir reinigen sämtliche Glasfronten und Variationen, vom Privathaushalt über Sc
         <div class="container">
           <h3 class="icon-contact">Kontakt</h3>
           <p>
-            Schicken Sie uns eine unverbindliche Anfrage und erhalten Sie ein Angebot von uns oder rufen Sie uns an unter 02203 20 16 74.
+            Schicken Sie uns eine unverbindliche Anfrage und erhalten Sie ein Angebot von uns oder rufen Sie uns an unter 02203/201674.
           </p>
           <form id="kontakt-form" action="index.php" method="POST">
             <div class="column">
@@ -193,7 +193,7 @@ Wir reinigen sämtliche Glasfronten und Variationen, vom Privathaushalt über Sc
                   <label for="sonstiges">Sonstiges</label>
                   <input name="sonstiges" id="sonstiges" type="text" />
 
-                  <label for="flaeche">geschätzte Fläche (qm)</label>
+                  <label for="flaeche">geschätzte Fläch * (qm)</label>
                   <input name="flaeche" id="flaeche" type="text" />
                 </div>
               </div>
@@ -232,7 +232,7 @@ Wir reinigen sämtliche Glasfronten und Variationen, vom Privathaushalt über Sc
   
             <div class="column">
               <div class="col-50-md">
-                <label for="firma">Firma</label>
+                <label for="firma">Firma*</label>
                 <input name="firma" id="firma" type="text" />
               </div>
               <div class="col-50-md">
@@ -240,27 +240,27 @@ Wir reinigen sämtliche Glasfronten und Variationen, vom Privathaushalt über Sc
                 <input name="abteilung" id="abteilung" type="text" />
               </div>
               <div class="col-50-md">
-                <label for="ansprechpartner">Ansprechpartner</label>
+                <label for="ansprechpartner">Ansprechpartner*</label>
                 <input name="ansprechpartner" id="ansprechpartner" type="text" />
               </div>
               <div class="col-50-md">
-                <label for="strasse">Straße</label>
+                <label for="strasse">Straße*</label>
                 <input name="strasse" id="strasse" type="text" />
               </div>
               <div class="col-50-md">
-                <label for="postleitzahl">Postleitzahl</label>
+                <label for="postleitzahl">Postleitzahl*</label>
                 <input name="postleitzahl" id="postleitzahl" type="text" />
               </div>
               <div class="col-50-md">
-                <label for="ort">Ort</label>
+                <label for="ort">Ort*</label>
                 <input name="ort" id="ort" type="text" />
               </div>
               <div class="col-50-md">
-                <label for="email">E-Mail</label>
+                <label for="email">E-Mail*</label>
                 <input name="email" id="email" type="text" />
               </div>
               <div class="col-50-md">
-                <label for="telefon">Telefon</label>
+                <label for="telefon">Telefon*</label>
                 <input name="telefon" id="telefon" type="text" />
               </div>
             </div>
@@ -284,13 +284,13 @@ Wir reinigen sämtliche Glasfronten und Variationen, vom Privathaushalt über Sc
           <h3 class="icon-about">Jobs</h3>
           
           <p>
-            Wir sind immer auf der Suche nach Qualifizierten, Engagierten und zuverlässigen Mitarbeitern.
+            Wir sind immer auf der Suche nach qualifizierten, engagierten und zuverlässigen Mitarbeitern.
           </p>
           <p>
-            Was wir erwarten? <strong>Zuverlässigkeit, Pünktlichkeit, Verantwortungsbewustsein, gepflegtes Äußeres, sicheres Auftreten, und ein Auge für die Reinigung. Putzen ist hier nicht Putzen.</strong> 
+            Was wir erwarten? <strong>Zuverlässigkeit, Pünktlichkeit, Verantwortungsbewusstsein, gepflegtes Äußeres, sicheres Auftreten, und ein Auge für die Reinigung. Reinigen ist nicht gleich Reinigen.</strong> 
           </p>
           <p>
-            Was wir bieten: <strong>Überdurchschnittliches Gehalt, Arbeiten mit den modernsten und besten Reinigungsmitteln, Interessante Reinigungslandschaften, ein freundliches Umfeld und ein Chef auf den man sich verlassen kann.</strong>
+            Was wir bieten: <strong>Überdurchschnittliches Gehalt, Arbeiten mit den modernsten und besten Reinigungsmitteln, interessante Reinigungslandschaften, ein freundliches Umfeld und ein Chef auf den man sich verlassen kann.</strong>
           </p>
           <p>
             Wir machen diese Berufung schon seit mehr als 10 Jahren und wissen wie wichtig engagierte und zuverlässige Mitarbeiter sind. Wenn Sie interessiert sind, einfach eine Email mit Ihrem Lebenslauf an
